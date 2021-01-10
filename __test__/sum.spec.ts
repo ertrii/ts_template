1cde669248b19b6ee7d69c65dd4c231618da1596
ertrii <ertriilive@gmail.com>
2021-01-09 19:15:34 -0500

    feat: gitignore and update vscode config

--- __test__/sum.spec.ts
+++ __test__/sum.spec.ts
@@ -1,7 +1,7 @@
 import sum from '../src/sum'
 
 describe('Sum', () => {
-  it('get result', () => {
-    expect(sum(15)).toBe(30)
-  })
+    it('get result', () => {
+        expect(sum(15)).toBe(30)
+    })
 })
